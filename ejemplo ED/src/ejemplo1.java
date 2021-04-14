715b098f9ad05c0b04336672656d56ca28f6ea96
Alumno <Alumno@A29-106>
2021-04-14 18:23:20 +0200

    SYSO

--- ejemplo ED/src/ejemplo1.java
+++ ejemplo ED/src/ejemplo1.java
@@ -1,4 +1,23 @@
 
 public class ejemplo1 {
+	
+	private int ejemplo1;
+	ejemplo1 (){
+		this.ejemplo1=0;
+	}
+	
+	public int getEjemplo1() {
+		return ejemplo1;
+	}
 
+	public void setEjemplo1(int ejemplo1) {
+		this.ejemplo1 = ejemplo1;
+	}
+
+	public static void main (String[]args) {
+		ejemplo1 hola= new ejemplo1();
+		System.out.println("hola paco");
+		System.out.println("*-*-*-*--**--**-*-*-");
+	}
+	
 }
